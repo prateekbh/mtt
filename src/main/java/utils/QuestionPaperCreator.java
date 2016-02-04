ee7e19e4747528c2d6e4fdd351e461bfebb4ec2d
Govardhan Reddy <govardhanreddy@govardhanreddy-macbookpro.roam.corp.google.com>
2016-02-05 04:57:10 +0530

    interm

--- src/main/java/utils/QuestionPaperCreator.java
+++ src/main/java/utils/QuestionPaperCreator.java
@@ -11,11 +11,12 @@ public class QuestionPaperCreator {
 
         for (int i = 0; i < MTT_CONSTANTS.NUMBER_OF_SETS; i++) {
             ArrayList<Integer> order = Sets.getOrderForCode(i + 1);
-            String systemCommand = "touch set" + (i + 1) + " ";
+            String set = "set" + (i + 1);
+            String systemCommand = "touch " + set + " ";
 //            System.out.println("order: " + order);
             int serialNum = 1;
             for (Integer q : order) {
-                systemCommand += " && echo -n " + serialNum + ". && cat " + q + " >> set" + (i + 1);
+                systemCommand += " && echo -n " + serialNum + ". >> " + set + " && cat " + q + " >> " + set;
             }
             System.out.println(systemCommand);
         }
