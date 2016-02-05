1c15ff3dec4fd9bec287d0606357f2c3d5d171d1
gopi6sigma <reddy6sigma@gmail.com>
2016-02-05 19:49:36 +0530

    set creator worked.

--- src/main/java/utils/QuestionPaperCreator.java
+++ src/main/java/utils/QuestionPaperCreator.java
@@ -9,10 +9,13 @@ import java.util.ArrayList;
 public class QuestionPaperCreator {
     public static void main(String[] args) {
 
+	String systemCommand = "";
         for (int i = 0; i < MTT_CONSTANTS.NUMBER_OF_SETS; i++) {
             ArrayList<Integer> order = Sets.getOrderForCode(i + 1);
-            String set = "set" + (i + 1);
-            String systemCommand = "touch " + set + " ";
+            String set = "set" + (i + 1) + ".txt";
+            systemCommand = " touch " + set + " ";
+	    systemCommand += " && cat header >> " + set + " && echo Question Paper Code: " + (i + 1) + " >> " + set;
+	    systemCommand += " && echo ' ' >> " + set;
 //            System.out.println("order: " + order);
             int serialNum = 1;
             for (Integer q : order) {
